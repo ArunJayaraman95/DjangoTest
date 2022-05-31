7b5b256349c6b9e045c03a445960e75f8d3f4ee0
Arun Jayaraman <riparun.aj@gmail.com>
2022-05-31 13:34:51 -0400

    Test Finish

--- FormTest/tests.py
+++ FormTest/tests.py
@@ -14,6 +14,28 @@ class TestForms(TestCase):
 
         self.assertTrue(form.is_valid())
 
+    def test_incorrect_datatype_number(self):
+        form = CartoonForm(data = {
+            "cartoon_name": "Hello",
+            "episode_count": 45,
+            "date_finished": "2000-01-01T12:00",
+            "rating": "Text"
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
+
+    def test_incorrect_datatype_date(self):
+        form = CartoonForm(data = {
+            "cartoon_name": "Hello",
+            "episode_count": 45,
+            "date_finished": "x",
+            "rating": 0.5
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 1)
+
     def test_episode_missing(self):
         form = CartoonForm(data = {
             "cartoon_name": "Hello",
@@ -24,7 +46,7 @@ class TestForms(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 1)
 
-    def test_cartoon_no_data(self):
+    def test_no_data(self):
         form = CartoonForm(data = {})
 
         self.assertFalse(form.is_valid())
